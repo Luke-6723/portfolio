e3aae3934839d59c1feb57a1d2b6b2b9ec236b34
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-16 17:41:13 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import './index.css'
 import App from './pages/App'
 import * as serviceWorker from './util/serviceWorker'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('main'))
 
 serviceWorker.unregister()
