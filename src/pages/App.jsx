40306a932981e37d801e662aa28c7e5d128adb11
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-16 17:41:18 +0100

    revert

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import NavBar from '../components/navbar'
 import '../css/App.css'
 
 function App () {
   return (
     <>
-      <NavBar />
       <div className='App'>
         <header className='App-header'>
           <p>
