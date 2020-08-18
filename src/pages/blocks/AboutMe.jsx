c2da49385846ec8dee217c826edb50fad7e3325b
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-18 17:45:50 +0100

    about me beginning

---
+++ src/pages/blocks/AboutMe.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 100vh;
+    background: rgb(55,55,55);
+`
+
+const AboutMe = () => {
+  return (
+    <Container />
+  )
+}
+
+export default AboutMe
