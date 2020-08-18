add22732370d91be34bdf7c91e1571ccf6c2f312
Luke-6723 <lukemorris2015@hotmail.com>
2020-08-18 17:45:26 +0100

    add a page store

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from './blocks/Main'
+import AboutMe from './blocks/AboutMe'
+
+const Home = () => {
+  return (
+    <>
+      <Main />
+      <AboutMe />
+    </>
+  )
+}
+
+export default Home
